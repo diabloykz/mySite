a5adfbd858e7e64dee780e768377a3142cd56a30
Thierry Poirine <tpoirine@gmail.com>
2019-06-17 11:24:26 -1000

    set params et preparation for deploy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :show, :update, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -8,7 +10,6 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -22,11 +23,9 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article édité avec succès"
       redirect_to article_path(@article)
@@ -36,7 +35,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "L'article a bien été supprimé"
     redirect_to articles_path
@@ -44,6 +42,9 @@ class ArticlesController < ApplicationController
 
 
   private
+  def set_article
+    @article = Article.find(params[:id])
+  end
   def article_params
     params.require(:article).permit(:title, :description)
   end
