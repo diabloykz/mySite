3d9e383cdbb9e3579a02cfcdb72ef8cea4664960
Thierry Poirine <tpoirine@gmail.com>
2019-06-20 14:35:08 -1000

    Ajout de la gemme i18n et localisation fr avec quelques tweaks

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module AlphaBlog
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    #config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    I18n.enforce_available_locales = false
+    config.i18n.available_locales = [:fr, :en]
+    I18n.default_locale = :fr
   end
 end
--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -14,3 +14,28 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym 'RESTful'
 # end
+
+ActiveSupport::Inflector.inflections(:fr) do |inflect|
+    inflect.clear
+
+    inflect.plural(/$/, 's')
+    inflect.singular(/s$/, '')
+
+    inflect.plural(/(bijou|caillou|chou|genou|hibou|joujou|pou|au|eu|eau)$/, '\1x')
+    inflect.singular(/(bijou|caillou|chou|genou|hibou|joujou|pou|au|eu|eau)x$/, '\1')
+
+    inflect.plural(/(bleu|émeu|landau|lieu|pneu|sarrau)$/, '\1s')
+    inflect.plural(/al$/, 'aux')
+    inflect.plural(/ail$/, 'ails')
+    inflect.singular(/(journ|chev)aux$/, '\1al')
+    inflect.singular(/ails$/, 'ail')
+
+    inflect.plural(/(b|cor|ém|gemm|soupir|trav|vant|vitr)ail$/, '\1aux')
+    inflect.singular(/(b|cor|ém|gemm|soupir|trav|vant|vitr)aux$/, '\1ail')
+
+    inflect.plural(/(s|x|z)$/, '\1')
+
+    inflect.irregular('monsieur', 'messieurs')
+    inflect.irregular('madame', 'mesdames')
+    inflect.irregular('mademoiselle', 'mesdemoiselles')
+  end
